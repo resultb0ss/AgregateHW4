fbe043523b78af4edd390f2dab9888d3b689c14e
Admin <reteamboss@gmail.com>
2024-09-15 14:12:32 +0300

    4

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
-    println("Hello World!")
+    val fruits = listOf("Яблоко","Абрикос","Банан","Виноград","Вишня","Кокос","яблоко")
+
+    println(fruits.filter { it.length % 2 == 0 }.groupBy { it.first().lowercase() })
+
+
+
+
 }
